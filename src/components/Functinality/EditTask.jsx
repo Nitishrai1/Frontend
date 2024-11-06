155187739e3087fc24bb2917b74ea529d97dccb9
Nitishrai1 <nitish.12208500@lpu.in>
2024-11-06 10:33:34 +0530

    update

--- src/components/Functinality/EditTask.jsx
+++ src/components/Functinality/EditTask.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
-
+const apiUrl= import.meta.env.VITE_API_URL;
 export default function Edittask() {
   const location = useLocation();
   const { id} = location.state || {};
